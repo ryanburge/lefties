2b14067be3e4981f40ec40de4cd6495bf073db90
Ryan Burge <ryanburge@gmail.com>
2016-04-23 22:04:42 -0500

    Starting to clean and merge the various datasets. Creating a single database

---
+++ reading_subsets.R
@@ -0,0 +1,26 @@
+setwd("D:/Baseball")
+
+library(lubridate)
+
+master <- read.csv("Master.csv", stringsAsFactors = FALSE)
+salary <- read.csv("salaries.csv", stringsAsFactors = FALSE)
+pitching <- read.csv("pitching.csv", stringsAsFactors = FALSE)
+
+master <- subset(master, throws == "L")
+master$finalGame <- as.Date(master$finalGame, "%m/%d/%Y")
+
+master <- subset(master, master$year >=2005)
+pitching <- subset(pitching, pitching$year >=2005)
+salary <- subset(salary, yearID >=2005)
+
+salary$year <- salary$yearID
+salary$player_id <- salary$playerID
+salary$yearID <- NULL
+salary$playerID <- NULL
+
+df <- merge(salary, pitching, by=c("year", "player_id"))
+
+master$player_id <- master$playerID
+
+df <- merge(df, master, by=c("year", "player_id"))
+
