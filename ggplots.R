c0c530c46d116834bc03b2023696a35b4cef2f30
Ryan Burge <ryanburge@gmail.com>
2016-04-26 07:10:20 -0500

    Added scatters. Figured out a color scheme.

--- ggplots.R
+++ ggplots.R
@@ -8,7 +8,9 @@ rht$throw <- 'right'
 lft$throw <- 'left'
 histogram<-rbind(rht,lft)
 
-ggplot(histogram, aes(x=Group.1, y = x/1000)) + geom_bar(aes(fill=throw),stat="identity", position= "dodge") + xlab("Season") + ylab("Salary (in thousands)")
+handPalette <- c("#daa520", "#228b22")
+
+ggplot(histogram, aes(x=Group.1, y = x/1000)) + geom_bar(aes(fill=throw),stat="identity", position= "dodge") + xlab("Season") + ylab("Salary (in thousands)") + scale_fill_manual(values=handPalette)
 
 
 l_al <- subset(lefties, lgID =="AL")
@@ -20,7 +22,9 @@ lnl$league <- "NL"
 lal$league <- "AL"
 leftleague<-rbind(lnl,lal)
 
-ggplot(leftleague, aes(x=Group.1, y = x/1000)) + geom_bar(aes(fill=league),stat="identity", position= "dodge") + xlab("Season") + ylab("Salary (in thousands)") + ggtitle("Lefties Salaries")
+leaguePalette <- c("#ff0000", "#0000cd")
+
+ggplot(leftleague, aes(x=Group.1, y = x/1000)) + geom_bar(aes(fill=league),stat="identity", position= "dodge") + xlab("Season") + ylab("Salary (in thousands)") + ggtitle("Lefties Salaries") + scale_fill_manual(values=leaguePalette)
 
 r_al <- subset(righties, lgID =="AL")
 ral <- aggregate(r_al$salary, list(r_al$year), mean, na.rm = TRUE)
@@ -30,6 +34,9 @@ rnl$league <- "NL"
 ral$league <- "AL"
 rightleague<-rbind(rnl,ral)
 
-ggplot(rightleague, aes(x=Group.1, y = x/1000)) + geom_bar(aes(fill=league),stat="identity", position= "dodge") + xlab("Season") + ylab("Salary (in thousands)") + ggtitle("Righties Salaries")
+ggplot(rightleague, aes(x=Group.1, y = x/1000)) + geom_bar(aes(fill=league),stat="identity", position= "dodge") + xlab("Season") + ylab("Salary (in thousands)") + ggtitle("Righties Salaries") + scale_fill_manual(values=leaguePalette)
 
+
+p <- ggplot(df, aes(salary/1000, era))
+p + geom_point(aes(colour = df$throw)) + xlim(5000, 25000) + scale_color_manual(values = c("#daa520", "#228b22")) + xlab("Salary (in thousands)") + ylab("ERA") +  theme(legend.title=element_blank())
 
